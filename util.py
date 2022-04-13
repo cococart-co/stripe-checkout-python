0d96576df85f742e50ef0dfb5e0b8d802ffd4063
Abhijeet-Agarwal <abhijeet@cococart.co>
2022-04-14 00:57:33 +0530

    Update comments in code file

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
 def getCheckoutSessionUrl():
+  ## Subscriptions with Stripe Checkout: https://stripe.com/docs/billing/subscriptions/checkout
   ## Your code here
-  return "Session url"
+  return "stripe session url"
