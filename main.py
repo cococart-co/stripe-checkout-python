3044c80a8d438f6de2826e21e5054ffd23edd42a
Abhijeet-Agarwal <abhijeet@cococart.co>
2022-04-13 15:50:54 +0530

    Add required constants

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask('app')
 
+stripeSecretKey = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
+subscriptionPriceId = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
+
 @app.route('/')
 def checkout():
   return 'Stripe checkout'
