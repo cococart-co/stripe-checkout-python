f2134958ea0a8b230b7c8cea8f394604149a3902
Abhijeet-Agarwal <abhijeet@cococart.co>
2022-04-14 01:03:45 +0530

    Final setup changes

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template
 from util import getCheckoutSessionUrl
 app = Flask('app')
 
-stripeSecretKey = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
-subscriptionPriceId = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
-
 @app.route('/')
 def checkout():
   url = getCheckoutSessionUrl()
--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+stripeSecretKey = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
+subscriptionPriceId = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
+
 def getCheckoutSessionUrl():
   ## Subscriptions with Stripe Checkout: https://stripe.com/docs/billing/subscriptions/checkout
   ## Your code here
