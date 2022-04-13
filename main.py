5f6cfedefe18945d98d1a2657ff6b48fe0da5f32
Abhijeet-Agarwal <abhijeet@cococart.co>
2022-04-14 00:50:23 +0530

    Use html to display the returned url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from util import getCheckoutSessionUrl
 app = Flask('app')
 
 stripeSecretKey = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
@@ -6,6 +7,7 @@ subscriptionPriceId = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
 
 @app.route('/')
 def checkout():
-  return 'Stripe checkout'
+  url = getCheckoutSessionUrl()
+  return render_template("index.html", url=url)
 
 app.run(host='0.0.0.0', port=8080)
