e33b1f74a35a2b4232f06db6dee309696b1a40ea
Abhijeet-Agarwal <abhijeet@cococart.co>
2022-04-14 14:14:32 +0530

    Follow PEP8 convention for naming

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from util import getCheckoutSessionUrl
+from util import get_checkout_session_url
 app = Flask('app')
 
 @app.route('/')
 def checkout():
-  url = getCheckoutSessionUrl()
+  url = get_checkout_session_url()
   return render_template("index.html", url=url)
 
 app.run(host='0.0.0.0', port=8080)
--- util.py
+++ util.py
@@ -1,7 +1,7 @@
-stripeSecretKey = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
-subscriptionPriceId = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
+stripe_secret_key = "sk_test_51IVvoGGpCUdxdLKw0LxioK1K3uR2b2tIZXIb3rP7tfotzdtPR7x5hINtPmm1EeIMdZpQRqu4SZgQsC30F62bsBPM00GZ97nIca"
+subscription_price_id = "price_1IVvz3GpCUdxdLKwnHD5YcfO"
 
-def getCheckoutSessionUrl():
+def get_checkout_session_url():
   ## Subscriptions with Stripe Checkout: https://stripe.com/docs/billing/subscriptions/checkout
   ## Your code here
   return "stripe session url"
